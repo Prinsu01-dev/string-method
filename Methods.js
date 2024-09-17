5f6c18336c86499c44196e695a6265342116ac12
Sachin Arya <sachin.arya@preplaced.in>
2024-09-17 14:20:21 +0530

    types of string methods

---
+++ Methods.js
@@ -0,0 +1,26 @@
+//**STRING METHODS//...
+
+
+// Uppercase and lowercase...
+var str = "PrashantAryas";
+var newStr = str.toUpperCase()//here is an other pproperty alson name is lowercase on the place of uppercase;
+
+//**str,slice(strat,end )
+var str = "Prashanta";
+console.log(str.slice(0, 4));
+
+//STRING CONCATINATION.....
+var str1 = "prashanta";
+var str2 = "arya";
+var res = str1.concat(str2);
+console.log(res);
+
+//string.replace......
+var str = "hello"
+console.log(str.replace("h", "y"));
+
+//**str.charAt()....
+
+var str = "Prashanta";
+console.log(str.charAt(7));
+
